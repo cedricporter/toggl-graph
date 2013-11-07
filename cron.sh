5ead81cf151d54b712aec9a612bf84f7e5f7a036
Cedric Porter <cedricporter@gmail.com>
2013-11-08 01:35:58 +0800

    add new cron file

---
+++ cron.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# File: cron.sh
+#
+# Created: 星期五, 十一月  8 2013 by Hua Liang[Stupid ET] <et@everet.org>
+#
+
+COUNTER=0
+while [  $COUNTER -lt 10 ]; do
+    echo The counter is $COUNTER
+    let COUNTER=COUNTER+1
+    curl $1
+    sleep $2
+done
