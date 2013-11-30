c00b06d832c1b242e26c95fbd060d620cd1b9e3b
Cedric Porter <cedricporter@gmail.com>
2013-11-30 11:40:53 +0800

    add file lock for rebuild_blog.sh

--- rebuild_blog.sh
+++ rebuild_blog.sh
@@ -1,8 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 #
 # File: rebuild_blog.sh
 #
 # Created: 星期日, 十一月 24 2013 by Hua Liang[Stupid ET] <et@everet.org>
 #
 
-cd /home/projects/my-summary && git pull && rake gen_deploy
+(
+    flock -x -w 10 345 || exit 1
+
+    cd /home/projects/my-summary && git pull && rake gen_deploy
+
+) 345>/var/lock/.$(basename $0).exclusivelock
